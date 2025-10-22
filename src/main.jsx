368f774f7f6fd870491805e47687ee67ba9eacd9
Luana Souza <luana@volxo.com.br>
2025-10-22 13:32:23 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import ConnectWhatsApp from "./ConnectWhatsApp.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/connect/:instanceName" element={<ConnectWhatsApp />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
